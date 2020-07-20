c860f1d36328167a3ce91bac1d6a40614649e539
MilePrastalo <mile.prastalo97@gmail.com>
2020-07-20 10:03:41 +0200

    created empty projects

---
+++ eeducation/src/main/kotlin/rs/eeducation/EeducationApplication.kt
@@ -0,0 +1,11 @@
+package rs.eeducation
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EeducationApplication
+
+fun main(args: Array<String>) {
+	runApplication<EeducationApplication>(*args)
+}
